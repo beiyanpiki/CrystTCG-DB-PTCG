126ce4a9e132be333b7d4e61701801012c302fc2
beiyanpiki <sizegaoxin@outlook.com>
2023-09-10 11:31:34 +0800

    firscommit

---
+++ src/main.py
@@ -0,0 +1,391 @@
+import json
+import shutil
+from pathlib import Path
+from typing import Optional, Tuple
+
+from src.model import PSet, Series, Label, CardType, Mechanic, CollectionAttr, Rarity, PokemonAttr, Energy, Stage, \
+    Ability, Resistance, Attack, Card, Weakness, EnergyAttr
+
+raw_json_path = '../PTCG-CHS-Datasets/ptcg_chs_infos.json'
+
+with open(raw_json_path, 'r') as f:
+    contents = f.read()
+    table = json.loads(contents)
+    collections = table['collections']
+    collections.reverse()
+    f.close()
+
+
+def get_series(series_id: str, set_name: str) -> Series:
+    match set_name:
+        case '特典卡·太阳&月亮':
+            series_id = '1'
+        case '2023宝可梦卡牌大师赛·广州 特典':
+            series_id = '1'
+        case '宝可梦卡牌超级赛·太阳&月亮 特典':
+            series_id = '1'
+        case '特典卡·剑&盾':
+            series_id = '2'
+        case '宝可梦卡牌超级赛·剑&盾 特典':
+            series_id = '2'
+        case '2023宝可梦卡牌大师赛·北京 特典':
+            series_id = '2'
+
+    if series_id == '1':
+        return Series.SM
+    elif series_id == '2':
+        return Series.SS
+    elif series_id == '3':
+        return Series.SV
+
+
+def get_card_type(card) -> CardType:
+    if card['cardType'] == '1':
+        return CardType.Pokemon
+    elif card['cardType'] == '2':
+        if card['details']['trainerType'] == '1':
+            if card['details']['ruleText'].startswith('宝可梦道具可以附着在自己的宝可梦身上'):
+                return CardType.Tool
+            else:
+                return CardType.Item
+        elif card['details']['trainerType'] == '2':
+            return CardType.Supporter
+        elif card['details']['trainerType'] == '3':
+            return CardType.Stadium
+    elif card['cardType'] == '3':
+        if card['energyType'] == '1':
+            return CardType.BasicEnergy
+        elif card['energyType'] == '2':
+            return CardType.SpecialEnergy
+
+    print('wtf??? No such card type')
+    exit(1)
+
+
+def get_mechanic(card) -> Optional[Mechanic]:
+    pt = [x for x in card.get('pokemonType', '').split('|') if x != '']
+    # V: only pokemon has V mechanic
+    if '1' in pt:
+        return Mechanic.V
+
+    # GX: only pokemon has GX mechanic
+    if '4' in pt:
+        return Mechanic.GX
+
+    # Prism_Star: All card can have Prism Star mechanic
+    if card.get('specialCard', None) == '4':
+        return Mechanic.Prism_Star
+
+    # Other mechanic(ex, Radiant, ACE SPEC) not in CHS card yet
+    return None
+
+
+def get_label(card) -> Optional[Label]:
+    # TAG TEAM: Pokemon and Supporter card can have TAG TEAM
+    if card.get('specialCard', None) == '5':
+        return Label.TAG_TEAM
+
+    # Ultra Beast: Only Pokemon has Ultra Beast
+    pt = [x for x in card.get('pokemonType', '').split('|') if x != '']
+    if '5' in pt:
+        return Label.Ultra_Beast
+
+    return None
+
+
+def get_rarity(card) -> Rarity:
+    rarity = card['details']['rarityText'].replace("☆", "")
+    match rarity:
+        case 'C':
+            return Rarity.Common
+        case 'U':
+            return Rarity.Uncommon
+        case 'R':
+            return Rarity.Rare
+        case 'PR':
+            return Rarity.PrismRare
+        case 'RR':
+            return Rarity.DoubleRare
+        case 'RRR':
+            return Rarity.TripleRare
+        case 'S':
+            return Rarity.Shiny
+        case 'SR':
+            return Rarity.SuperRare
+        case 'SSR':
+            return Rarity.ShinySuperRare
+        case 'CHR':
+            return Rarity.CharacterRare
+        case 'A':
+            return Rarity.AmazingRare
+        case 'HR':
+            return Rarity.HyperRare
+        case 'UR':
+            return Rarity.UltraRare
+        case '无标记':
+            return Rarity.NoLabel
+        case '_':
+            input('WTF???')
+            exit(1)
+
+
+def get_card_in_coll(card) -> Tuple[str | None, str | None]:
+    coll_no = card['details']['collectionNumber'].split('/')
+    if len(coll_no) == 2:
+        return coll_no[0], coll_no[1]
+    else:
+        if coll_no[0] in ['DAR', 'FAI', 'FIG', 'FIR', 'GRA', 'LIG', 'MET', 'PSY', 'WAT']:
+            return coll_no[0], None
+        elif coll_no[0] in ['SM-P', 'S-P']:
+            return None, coll_no[0]
+        else:
+            return None, None
+
+
+def get_energy_by_eid_yorenid(eid: str) -> Energy:
+    match eid:
+        case '1' | 'Y457':
+            return Energy.Grass
+        case '2' | 'Y458':
+            return Energy.Fire
+        case '3' | 'Y459':
+            return Energy.Water
+        case '4' | 'Y460':
+            return Energy.Lightning
+        case '5' | 'Y461':
+            return Energy.Psychic
+        case '6' | 'Y462':
+            return Energy.Fighting
+        case '7' | 'Y463':
+            return Energy.Darkness
+        case '8' | 'Y464':
+            return Energy.Metal
+        case '9' | 'Y465':
+            return Energy.Fairy
+        case '10':
+            return Energy.Dragon
+        case '11':
+            return Energy.Colorless
+        case 'none':
+            return Energy.Zero
+        case '12':
+            return Energy.Addition
+        case _:
+            print("Error: Not valid eid or yorenId")
+            exit(1)
+
+
+def get_stage(card) -> Stage:
+    match card['details']['evolveText']:
+        case '基础':
+            return Stage.Basic
+        case '1阶进化':
+            return Stage.Stage1
+        case '2阶进化':
+            return Stage.Stage2
+        case 'V进化':
+            return Stage.VMAX
+        case _:
+            print('wtf????')
+            exit(1)
+
+
+def get_pokemon_attr(card) -> PokemonAttr:
+    assert card['cardType'] == '1'
+
+    energy_type = card['details']['attribute']
+    energy_type = get_energy_by_eid_yorenid(energy_type)
+    stage = get_stage(card)
+    hp = card['details']['hp']
+
+    ability = (card['details'].get('featureName', None), card['details'].get('featureText', None))
+    if ability[0] is not None:
+        ability = Ability(ability[0], ability[1])
+    else:
+        ability = None
+    ancient_trait = None
+
+    weakness_type = card['details'].get('weaknessType', None)
+    weakness_value = card['details'].get('weaknessFormula', None)
+    if weakness_type is not None:
+        weakness = Weakness(get_energy_by_eid_yorenid(weakness_type), weakness_value)
+    else:
+        weakness = None
+
+    resistance = (card['details'].get('resistanceType', None), card['details'].get('resistanceFormula', None))
+    if resistance[0] is not None:
+        resistance = Resistance(get_energy_by_eid_yorenid(resistance[0]), resistance[1])
+    else:
+        resistance = None
+
+    retreat_cost = card['details'].get('retreatCost', 0)
+    pokedex = card['details'].get('yorenCode', None)
+
+    attacks = []
+    for attack in card['details']['abilityItemList']:
+        atk_name = attack['abilityName']
+        atk_text = attack['abilityText']
+        atk_cost = attack.get('abilityCost', '')
+        atk_cost = ['none'] if atk_cost == '' else [elem for elem in atk_cost.split(',') if elem != ""]
+        for i in range(len(atk_cost)):
+            e = get_energy_by_eid_yorenid(atk_cost[i])
+            atk_cost[i] = e
+        atk_damage = None if attack['abilityDamage'] == 'none' else attack['abilityDamage']
+        atk = Attack(atk_name, atk_text, atk_cost, atk_damage)
+        attacks.append(atk)
+    return PokemonAttr(energy_type, stage, hp, ability, ancient_trait, weakness, resistance, retreat_cost, pokedex,
+                       attacks)
+
+
+def get_card_text(card, card_type) -> str:
+    res = ''
+    if card_type == CardType.Pokemon:
+        if 'featureName' in card['details']:
+            res += f"{card['details']['featureName']}\n{card['details']['featureText']}\n"
+        for attack in card['details']['abilityItemList']:
+            if 'abilityText' in attack:
+                res += f"{attack['abilityName']}\t{attack['abilityDamage'] if attack['abilityDamage'] != 'none' else ''}\n{attack['abilityText'] if attack['abilityText'] != 'none' else ''}\n"
+    elif card_type != CardType.BasicEnergy:
+        r = card['details']['ruleText']
+        r = r.replace("|在自己的回合可以使用任意张物品卡。", '')
+        r = r.replace('宝可梦道具可以附着在自己的宝可梦身上。每只宝可梦身上只可以附着1张宝可梦道具，并保持附加状态。|', '')
+        r = r.replace('|在自己的回合只可以使用1张支援者卡。', '')
+        r = r.replace(
+            '|在自己的回合只可以将1张竞技场卡放到战斗区旁。如果有别的名称的竞技场卡被放入场上，则将此卡放入弃牌区。', '')
+        r = r.replace('|在1副卡组中只能放入1张同名的◇（棱镜之星）卡。这张卡牌不会被放入弃牌区，而会被放入放逐区。', '')
+        r = r.replace('|在自己的回合只可以使用1张支援者卡', '')
+        r = r.replace('|', '')
+        res += f"{r}\n"
+    else:
+        res = ''
+    return res
+
+
+effect_dic = []
+
+
+def sort_cards_by_card_no(card: Card):
+    card_no = card.collection_attr.card_no
+    if card_no.isdigit():
+        return 0, int(card_no)
+    elif card_no.startswith("NaN"):
+        return 1, int(card_no[3:])
+    else:
+        return 2, card_no
+
+
+def main():
+    database = {}
+    for collection in collections:
+        set_name = collection['name']
+        set_symbol = collection['commodityCode']
+        set_publish_date = collection['salesDate']
+        set_series_id = collection.get('series', None)
+        set_series = get_series(set_series_id, set_name)
+
+        sets = PSet(set_name, set_symbol, set_publish_date, set_series)
+
+        for card in collection['cards']:
+            if card['details']['rarityText'].find('☆') != -1:
+                continue
+
+            name = card['name']
+            card_type = get_card_type(card)
+            card_text = get_card_text(card, card_type)
+            mechanic = get_mechanic(card)
+            label = get_label(card)
+
+            card_idx, coll_num = get_card_in_coll(card)
+            artist = card['details'].get('illustratorName', [None])[0]
+            rarity = get_rarity(card)
+            collect_attr = CollectionAttr(set_series, set_symbol, coll_num, card_idx, artist, rarity)
+            regulation_mark = card['details'].get('regulationMarkText', None)
+            pokemon_attr = get_pokemon_attr(card) if card_type == CardType.Pokemon else None
+            energy_attr = EnergyAttr(
+                get_energy_by_eid_yorenid(card['details']['yorenCode'])
+            ) if card_type == CardType.BasicEnergy else None
+
+            card_after = Card(name, card_text, card_type, mechanic, label, pokemon_attr, collect_attr, energy_attr,
+                              regulation_mark)
+            card_after.img_path = card['image']
+            sets.cards.append(card_after)
+        database[set_symbol] = sets
+
+    database["SMP"] = database.pop("PROMO")
+    database['SSP'] = database.pop("PROMO3")
+
+    # Combine PROMO
+    cnt = 0
+    for k, v in database.items():
+        if k in ['PROMO1', 'PROMO2']:
+            for card in v.cards:
+                c = card
+                if c.collection_attr.card_no is None:
+                    cnt += 1
+                    c.collection_attr.card_no = f'NaN{cnt}'
+                database['SMP'].cards.append(c)
+
+    cnt = 0
+    for k, v in database.items():
+        if k in ['PROMO4', 'PROMO6']:
+            for card in v.cards:
+                c = card
+                if c.collection_attr.card_no is None:
+                    cnt += 1
+                    c.collection_attr.card_no = f'NaN{cnt}'
+                database['SSP'].cards.append(c)
+
+    del database['PROMO1']
+    del database['PROMO2']
+    del database['PROMO4']
+    del database['PROMO6']
+
+    for k in database.keys():
+        if database[k].symbol.find('PROMO') != -1:
+            database[k].symbol = 'PROMO'
+    database["SSP"].symbol = 'SVP'
+    database["SSP"].name = "剑&盾 特典卡"
+    database["SMP"].symbol = 'SMP'
+    database["SMP"].name = '太阳&月亮 特典卡'
+
+    return database
+
+
+def custom_encoder(obj):
+    if hasattr(obj, '__json__'):
+        return obj.__json__()
+    else:
+        raise TypeError(f'Object of type {type(obj)} is not JSON serializable')
+
+
+def convert_to_json(obj, compress=False):
+    return json.dumps(obj, default=custom_encoder, indent=4) if not compress else json.dumps(obj,
+                                                                                             default=custom_encoder,
+                                                                                             separators=(',', ':'))
+
+
+if __name__ == '__main__':
+    data = main()
+
+    sets = []
+    with open('../output/cards.json', 'w') as f:
+        f.write(convert_to_json(data))
+    with open('../output/cards_min.json', 'w') as f:
+        f.write(convert_to_json(data, compress=True))
+
+    for k, v in data.items():
+        folder = Path(f'../output/img/{k}')
+        folder.mkdir(parents=True, exist_ok=True)
+        for card in v.cards:
+            src = f'../PTCG-CHS-Datasets/{card.img_path}'
+            dst = f'../output/img/{k}/{card.collection_attr.card_no}.jpg'
+            shutil.copy2(src, dst)
+
+        v.cards = []
+        sets.append(v)
+
+    with open('../output/sets.json', 'w') as f:
+        f.write(convert_to_json(sets))
+
+    with open('../output/sets_min.json', 'w') as f:
+        f.write(convert_to_json(sets, compress=True))
---
+++ src/model.py
@@ -0,0 +1,344 @@
+import hashlib
+from enum import Enum
+from typing import List, Optional
+
+
+class Series(Enum):
+    SM = 'Sun & Moon'
+    SS = 'Sword & Shield'
+    SV = 'Scarlet & Violet'
+
+
+class SetType(Enum):
+    MainExpansion = '0'
+    SideProduct = '1'
+
+
+class CardType(Enum):
+    Pokemon = 'Pokemon'
+    Trainer = 'Trainer'
+    Energy = 'Energy'
+    Item = 'Item'
+    Supporter = 'Supporter'
+    Stadium = 'Stadium'
+    Tool = 'Pokemon Tool'
+    BasicEnergy = 'Basic Energy'
+    SpecialEnergy = 'Special Energy'
+
+
+class Mechanic(Enum):
+    ex = 'ex'
+    V = 'V'
+    GX = 'GX'
+    Radiant = 'Radiant'
+    Prism_Star = 'Prism Star'
+    ACE_SPEC = 'ACE SPEC'
+
+
+class Label(Enum):
+    TAG_TEAM = 'TAG TEAM'
+    Ultra_Beast = 'Ultra Beast'
+    Fusion_Strike = 'Fusion Strike'
+    Single_Strike = 'Single Strike'
+    Rapid_Strike = 'Rapid Strike'
+
+
+class Rarity(Enum):
+    Common = 'C'
+    Uncommon = 'U'
+    Rare = 'R'
+    PrismRare = 'PR'
+    DoubleRare = 'RR'
+    TripleRare = 'RRR'
+    Shiny = 'S'
+    SuperRare = 'SR'
+    ShinySuperRare = 'SSR'
+    CharacterRare = 'CHR'
+    AmazingRare = 'A'
+    HyperRare = 'HR'
+    UltraRare = 'UR'
+    NoLabel = ''
+
+
+class Energy(Enum):
+    Grass = 'G'
+    Fire = 'R'
+    Water = 'W'
+    Lightning = 'L'
+    Psychic = 'P'
+    Fighting = 'F'
+    Darkness = 'D'
+    Metal = 'M'
+    Fairy = 'Y'
+    Dragon = 'N'
+    Colorless = 'C'
+
+    Zero = '0'
+    Addition = '+'
+
+
+class Stage(Enum):
+    Basic = 'Basic'
+    Stage1 = 'Stage 1'
+    Stage2 = 'Stage 2'
+    VMAX = 'VMAX'
+    VSTAR = 'VSTAR'
+    V_UNION = 'V-UNION'
+
+
+class AncientTrait(Enum):
+    Tera = 'Tera'
+
+
+class Resistance:
+    resistance_type: Energy
+    resistance_value: str
+
+    def __init__(self, resistance_type: Energy, resistance_value: str) -> None:
+        self.resistance_type = resistance_type
+        self.resistance_value = resistance_value
+
+    def __json__(self):
+        return {
+            "resistance_type": self.resistance_type.value,
+            "resistance_value": self.resistance_value
+        }
+
+
+class Weakness:
+    weakness_type: Energy
+    weakness_value: str
+
+    def __init__(self, weakness_type: Energy, weakness_value: str) -> None:
+        self.weakness_type = weakness_type
+        self.weakness_value = weakness_value
+
+    def __json__(self):
+        return {
+            "weakness_type": self.weakness_type.value,
+            "weakness_value": self.weakness_value
+        }
+
+
+class Attack:
+    name: str
+    text: str
+    cost: List[Energy]
+    damage: Optional[str]
+
+    def __init__(self,
+                 name: str,
+                 text: str,
+                 cost: List[Energy],
+                 damage: Optional[str]) -> None:
+        self.name = name
+        self.text = text
+        self.cost = cost
+        self.damage = damage
+
+    def __json__(self):
+        return {
+            'name': self.name,
+            'text': self.text,
+            'cost': [energy.value for energy in self.cost],
+            'damage': self.damage,
+        }
+
+
+class Ability:
+    name: str
+    text: str
+
+    def __init__(self, name: str, text: str) -> None:
+        self.name = name
+        self.text = text
+
+    def __json__(self):
+        return {
+            'name': self.name,
+            'text': self.text,
+        }
+
+
+class PokemonAttr:
+    energy_type: Energy
+    stage: Stage
+    hp: int
+    ability: Optional[Ability]
+    ancient_trait: Optional[AncientTrait]
+    weakness: Optional[Weakness]
+    resistance: Optional[Resistance]
+    retreat_cost: int
+    pokedex: str
+    attacks: List[Attack]
+
+    def __init__(self,
+                 energy_type: Energy,
+                 stage: Stage,
+                 hp: int,
+                 ability: Optional[Ability],
+                 ancient_trait: Optional[AncientTrait],
+                 weakness: Optional[Weakness],
+                 resistance: Optional[Resistance],
+                 retreat_cost: int,
+                 pokedex: str,
+                 attacks: List[Attack],
+                 ):
+        self.energy_type = energy_type
+        self.stage = stage
+        self.hp = hp
+        self.ability = ability
+        self.ancient_trait = ancient_trait
+        self.weakness = weakness
+        self.resistance = resistance
+        self.retreat_cost = retreat_cost
+        self.pokedex = pokedex
+        self.attacks = attacks
+
+    def __json__(self):
+        return {
+            'energy_type': self.energy_type.value,
+            'stage': self.stage.value,
+            'hp': self.hp,
+            'ability': self.ability.__json__() if self.ability else None,
+            'ancient_trait': self.ancient_trait.value if self.ancient_trait else None,
+            'weakness': self.weakness.__json__() if self.weakness else None,
+            'resistance': self.resistance.__json__() if self.resistance else None,
+            'retreat_cost': self.retreat_cost,
+            'pokedex': self.pokedex,
+            'attacks': [attack.__json__() for attack in self.attacks]
+        }
+
+
+class EnergyAttr:
+    energy: Energy
+
+    def __init__(self, energy) -> None:
+        self.energy = energy
+
+    def __json__(self):
+        return {
+            "energy": self.energy.value
+        }
+
+
+class CollectionAttr:
+    series: Series
+    set_symbol: str
+    # card_no / set_collect_num
+    card_no: Optional[str]
+    set_collect_num: Optional[str]
+    artist_name: Optional[str]
+    rarity: Rarity
+
+    def __init__(self, series: Series, set_symbol: str, set_collect_num: str, card_no: str, artist_name: str,
+                 rarity: Rarity) -> None:
+        self.series = series
+        self.set_symbol = set_symbol
+        self.set_collect_num = set_collect_num
+        self.card_no = card_no
+        self.artist_name = artist_name
+        self.rarity = rarity
+
+    def __json__(self):
+        return {
+            'series': self.series.value,
+            'set_symbol': self.set_symbol,
+            'card_no': self.card_no,
+            'set_collect_num': self.set_collect_num,
+            'artist_name': self.artist_name,
+            'rarity': self.rarity.value
+        }
+
+
+class Card:
+    name: str
+    text: str
+    type: CardType
+    mechanic: Mechanic
+    label: Optional[Label]
+    pokemon_attr: Optional[PokemonAttr]
+    energy_attr: Optional[EnergyAttr]
+    collection_attr: CollectionAttr
+    regulation_mark: Optional[str]
+    img_path: str
+    effect_id: str
+
+    def __init__(self,
+                 name: str,
+                 text: str,
+                 ctype: CardType,
+                 mechanic: Optional[Mechanic],
+                 label: Optional[Label],
+                 pokemon_attr: Optional[PokemonAttr],
+                 collection_attr: CollectionAttr,
+                 energy_attr: Optional[EnergyAttr],
+                 regulation_mark: Optional[str],
+                 ) -> None:
+        self.name = name
+        self.text = text
+        self.type = ctype
+        self.mechanic = mechanic
+        self.label = label
+        self.pokemon_attr = pokemon_attr
+        self.collection_attr = collection_attr
+        self.regulation_mark = regulation_mark
+        self.energy_attr = energy_attr
+
+        # EffectId
+        if self.type == CardType.Pokemon:
+            self.effect_id = (f"{self.name}|"
+                              f"{self.pokemon_attr.stage.value}|"
+                              f"{self.pokemon_attr.hp}|"
+                              f"{self.pokemon_attr.ability.name if self.pokemon_attr.ability else ''}|"
+                              f"{[attack.name for attack in self.pokemon_attr.attacks]}|"
+                              f"{self.pokemon_attr.weakness.weakness_type if self.pokemon_attr.weakness else ''}|"
+                              f"{self.pokemon_attr.weakness.weakness_value if self.pokemon_attr.weakness else ''}|"
+                              f"{self.pokemon_attr.resistance.resistance_type.value if self.pokemon_attr.resistance else ''}|"
+                              f"{self.pokemon_attr.resistance.resistance_value if self.pokemon_attr.resistance else ''}|"
+                              f"{self.pokemon_attr.retreat_cost}")
+        else:
+            self.effect_id = f"{self.name}"
+        self.effect_id = hashlib.md5(self.effect_id.encode()).hexdigest()
+
+    def __json__(self):
+        return {
+            'name': self.name,
+            'text': self.text,
+            'type': self.type.value,
+            'mechanic': self.mechanic.value if self.mechanic else None,
+            'label': self.label.value if self.label else None,
+            'pokemon_attr': self.pokemon_attr.__json__() if self.pokemon_attr else None,
+            'collection_attr': self.collection_attr.__json__(),
+            'energy_attr': self.energy_attr.__json__() if self.energy_attr else None,
+            'regulation_mark': self.regulation_mark,
+            'effect_id': self.effect_id
+        }
+
+
+class PSet:
+    name: str
+    symbol: str
+    release_date: Optional[str]
+    series: Series
+    cards: List[Card]
+
+    def __init__(self, name: str, symbol: str, release_date: Optional[str],
+                 series_id: Series) -> None:
+        self.name = name
+        self.symbol = symbol
+        self.release_date = release_date
+        self.series = series_id
+        self.cards = []
+
+    def __json__(self):
+        data = {
+            'name': self.name,
+            'symbol': self.symbol,
+            'release_date': self.release_date,
+            'series': self.series.value,
+            'cards': [card.__json__() for card in self.cards]
+        }
+        if len(data['cards']) == 0:
+            del data['cards']
+        return data
