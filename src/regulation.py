58ff187e6ca9f2f8470644f9d19566f11ab0e841
beiyanpiki <sizegaoxin@outlook.com>
2023-12-05 03:59:28 +0800

    feat: fix

--- src/regulation.py
+++ src/regulation.py
@@ -5,24 +5,28 @@ exp_banned = [
     ['CSM1aC', '128'],
     ['CSM1cC', '137'],
 ]
+exp_banned_effect = []
 
 with open('../output/sets_min.json', 'r') as f:
     contents = f.read()
     table = json.loads(contents)
     f.close()
 
-banned_effect = []
 for ban_card in exp_banned:
     found = False
     for coll in table:
         for card in coll['cards']:
             if card['collection_attr']['set_symbol'] == ban_card[0] and card['collection_attr']['card_no'] == ban_card[
                 1]:
-                banned_effect.append(card['effect_id'])
+                exp_banned_effect.append(card['effect_id'])
                 found = True
                 break
         if found:
             break
 
-with open('../output/exp_banned.json', 'w') as f:
-    f.write(json.dumps(banned_effect, separators=(',', ':')))
+with open('../output/banned.json', 'w') as f:
+    f.write(json.dumps({
+        'std': [],
+        'exp': exp_banned_effect,
+        'sm': [],
+    }, separators=(',', ':')))
